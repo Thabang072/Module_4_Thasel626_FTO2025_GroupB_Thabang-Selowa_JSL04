3ec4516dbe8f45fdd514bfcfbe916ce16b868873
Thabang Selowa <mohale20cobra@gmail.com>
2024-07-19 00:49:59 +0200

    Add Stock check tracker

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ if (userPreference === '') {
 // You have a variable that tracks the number of Monster Energy cans left in the fridge. 
 // Use a ternary operator to print "Time to restock!" if the number is less than 5, or "We're stocked!" if the number is 5 or more.
 
-let cansLeft = 3; // Any number of cans
+let cansLeft = 7; // Any number of cans
 if (cansLeft <= 5){
 console.log(`Time to restock!`);
 } else if (cansLeft > 5) {
